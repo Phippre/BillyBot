974092e05bece0c79d20ff501f6925d196f2ff19
Phippre <parkercantor29@gmail.com>
2021-05-22 11:17:28 -0400

    Youtube Overhaul

---
+++ src/Cogs/PlayYoutube.py
@@ -0,0 +1,75 @@
+import discord
+import random
+import time
+import os
+import youtube_dl
+from discord import channel
+from discord import FFmpegPCMAudio
+from discord import guild
+from discord.ext import commands, tasks
+from itertools import cycle
+from discord.ext.commands import context
+from discord.ext.commands.context import Context
+from discord.guild import Guild
+from discord.member import VoiceState
+from discord.utils import escape_markdown
+from asyncio.tasks import sleep
+from typing import ContextManager
+
+from youtube_dl.YoutubeDL import YoutubeDL
+
+class PlayYoutube(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Youtube Cog ready.')
+
+    @commands.command()
+    async def play(self, ctx, *, url):
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
+        voice = await self.client.get_channel(533319670552330244).connect()
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download = False)
+            I_URL = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(I_URL, **FFMPEG_OPTIONS)
+            voice.play(source)
+            voice.is_playing()
+
+def setup(client): 
+    client.add_cog(PlayYoutube(client))
+
+
+
+
+
+
+######################################
+                # Previous Play Code #
+#song_there = os.path.isfile('./src/Cogs/song.mp3')
+#        
+#        if song_there:
+#            os.remove('./src/Cogs/song.mp3')
+#
+#        vc = await self.client.get_channel(533319670552330244).connect()
+#
+#        ydl_opts = {
+#            'format': 'bestaudio/best',
+#            'postprocessors': [{
+#                'key': 'FFmpegExtractAudio',
+#                'preferredcodec': 'mp3',
+#                'preferredquality': '192',
+#            }],
+#        }
+#
+#        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#            ydl.download([url])
+#
+#        for file in os.listdir('./'):
+#            if file.endswith('.mp3'):
+#                os.rename(file, './src/Cogs/song.mp3')
+#                print('File succesfully downloaded, renamed and moved folders. Attempting to play.')
+#        vc.play(discord.FFmpegPCMAudio(executable='C:/ffmpeg/bin/ffmpeg.exe', source='./src/Cogs/song.mp3'))
--- src/main.py
+++ src/main.py
@@ -22,19 +22,22 @@ intents.members = True
 client = commands.Bot(command_prefix='.', intents = intents, help_command=None)
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension): 
     client.load_extension(f'Cogs.{extension}')
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def unload(ctx, extension): 
     client.unload_extension(f'Cogs.{extension}')
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def reload(ctx, extension): 
     client.unload_extension(f'Cogs.{extension}')
     client.load_extension(f'Cogs.{extension}')
 
-for filename in os.listdir('C:/Users/parke/Documents/GitHub/BillyBot/src/Cogs'): 
+for filename in os.listdir('C:/Users/parke/Documents/GitHub/BillyBot/src/Cogs'):
     if filename.endswith('.py'): 
         client.load_extension(f'Cogs.{filename[:-3]}') 
 
