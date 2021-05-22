ac77f9226a62d25074a05d54fbed68804e619477
Phippre <parkercantor29@gmail.com>
2021-05-21 20:21:01 -0400

    Randomized sound clips

--- src/Cogs/BasicCommands.py
+++ src/Cogs/BasicCommands.py
@@ -25,10 +25,7 @@ class BasicCommands(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         await self.client.change_presence(activity=discord.Game('With Cocaine'))
-        #global currentMembers
-        #currentMembers = len(self.client.get_channel(533319670552330244).members)
         print('Billy is ready for you daddy.')
-        #self.detect_members.start()
 
     @commands.command()
     async def help(self, ctx):
@@ -80,23 +77,6 @@ class BasicCommands(commands.Cog):
     @commands.command()
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
-
-    #@tasks.loop()
-    #async def detect_members(self):
-    #    global currentMembers
-    #    newChannelMembers = self.client.get_channel(533319670552330244).members
-    #    
-    #    if len(newChannelMembers) != currentMembers and len(newChannelMembers) >= currentMembers:
-    #        currentChannel = await self.client.get_channel(533319670552330244).connect()
-    #        currentChannel.play(discord.FFmpegPCMAudio(executable='C:/ffmpeg/bin/ffmpeg.exe', source='C:/hahh.mp3'))
-    #        while currentChannel.is_playing():
-    #            time.sleep(.1)
-    #        else:
-    #            await currentChannel.disconnect()
-    #        
-    #    newChannelMembers = self.client.get_channel(533319670552330244).members
-    #
-    #    currentMembers = len(newChannelMembers)
 
     
 
--- src/Cogs/DetectMembers.py
+++ src/Cogs/DetectMembers.py
@@ -27,22 +27,31 @@ class DetectMembers(commands.Cog):
     async def on_ready(self):
 
         global currentMembers
+        global soundClips
+
+        soundClips = []
+
+        for filename in os.listdir('C:/SoundClips'):
+            if filename.endswith('.mp3'):
+                soundClips.append(filename)
+
         currentMembers = len(self.client.get_channel(533319670552330244).members)
 
         print('Detect Members Ready.')
-        
+
         self.detect_members.start()
 
     @tasks.loop()
     async def detect_members(self):
 
         global currentMembers
+        global soundClips
 
         newChannelMembers = self.client.get_channel(533319670552330244).members
         
         if len(newChannelMembers) != currentMembers and len(newChannelMembers) >= currentMembers:
             currentChannel = await self.client.get_channel(533319670552330244).connect()
-            currentChannel.play(discord.FFmpegPCMAudio(executable='C:/ffmpeg/bin/ffmpeg.exe', source='C:/hahh.mp3'))
+            currentChannel.play(discord.FFmpegPCMAudio(executable='C:/ffmpeg/bin/ffmpeg.exe', source=f'C:/SoundClips/{random.choice(soundClips)}'))
             while currentChannel.is_playing():
                 time.sleep(.1)
             else:
